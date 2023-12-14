8beac0992eb08f84e0df3da38f9d58e14c005075
reo106 <k21069kk@aitech.ac.jp>
2023-12-14 16:09:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.ReoDiary import ReoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
-
+diaries = [
+    DiarySample(), 
+    ReoDiary(),
+]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
