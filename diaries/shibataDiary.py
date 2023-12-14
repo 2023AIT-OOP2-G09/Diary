fa1c90b7809e38f39a9ac7d9753a6af3a9108af3
shiruku-shibata <k22065kk@aitech.ac.jp>
2023-12-14 18:28:37 +0900

    shibataDiaryの追加

---
+++ diaries/shibataDiary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class shibataDiary (AbstractDiary):
+    def get_date (self):
+        return "2020-12-14"
+    
+    def get_summary (self):
+        return "リーダーになったけど不安しかない"
+    
+    def get_author (self):
+        return "shibata"
