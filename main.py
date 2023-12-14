d9c9eaebddeb8817d96fa73d9e353a5b6f89d43c
DDD114ts <gt03ke2sk@gmail.com>
2023-12-14 16:16:08 +0900

    後藤日記

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from diaries.DiarySample import DiarySample
-
+from diaries.GotoDiary import GotoDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),GotoDiary()]
 
 for d in diaries:
     print("---------------------------------")
